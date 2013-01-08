c749cae3f41b5142abd6db1ede99b1e3e7647efd
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2013-01-08 16:42:45 +0100

    Display Vainamoinen status on home page - rudimentary

--- kantele/views.py
+++ kantele/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render, HttpResponse
+import json
 
 def home(request):
-    return render(request, 'kantele/index.html')
+    try:
+        with open('infofile_status.json') as fp:
+            status = json.load(fp)
+    except:
+        pass # FIXME logging
+
+    return render(request, 'kantele/index.html', {'status': status} )
