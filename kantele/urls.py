3ee62bccfc35737c35918a854bb78a67ee634093
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2013-01-15 13:58:29 +0100

    Changed urls and added new and edit views

--- kantele/urls.py
+++ kantele/urls.py
@@ -7,15 +7,25 @@ admin.autodiscover()
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^kantele/$', 'kantele.views.home'),
-    url(r'^kantele/newdataset/$', 'metadata.views.new_dataset'),
-    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^kantele/login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'metadata.views.logout'),
+    
+    url(r'^kantele/$', 'kantele.views.home'),
+    
+    url(r'^kantele/dataset/(?P<dataset_id>\w+)/$', 'metadata.views.show_dataset'),
+
+    url(r'^kantele/dataset/new/$', 'metadata.views.new_dataset'),
+    url(r'^kantele/dataset/edit/(?P<dataset_id>\w+)/$', 'metadata.views.edit_dataset'),
+
+    url(r'^kantele/dataset/files/(?P<dataset_id>\w+)/$', 'metadata.views.add_files'),
+    url(r'^kantele/dataset/metadata/(?P<dataset_id>\w+)/$', 'metadata.views.write_metadata'),
+    url(r'^kantele/dataset/outliers/(?P<dataset_id>\w+)/$', 'metadata.views.define_outliers'),
+
     url(r'^kantele/store_metadata/$', 'metadata.views.store_dataset'),
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
--- metadata/newviews.py
+++ metadata/newviews.py
@@ -8,11 +8,21 @@ import metadata
 empty_mds = metadata.MetadataSet()
 
 
-# should prob be in util module
+@login_required
+def new_dataset(request):
+    mds = copy.deepcopy(empty_mds)
+    oid = mds.initialize_new_dataset()
+    # FIXME store oid also in sql
+    redirect('/kantele/files/{0}'.format(str(oid)) )
 
-
-
-########################
+@login_required
+def edit_dataset(request, dataset_id):
+    mds = copy.deepcopy(empty_mds)
+    mds.load_from_db(dataset_id)
+    mds.update_db_status('tmp')
+    mds.create_draft_from_metadata()
+    redirect('/kantele/dataset/{0}'.format(dataset_id))
+    
 
 @login_required
 def show_dataset(request, dataset_id):
@@ -23,7 +33,7 @@ def show_dataset(request, dataset_id):
 
 
 @login_required
-def add_files(request, dataset_id):
+def add_files(request, dataset_id):    
     dataset_view_action(request, dataset_id, 'file_input.html', 'metadata')
 
 
@@ -51,15 +61,16 @@ def dataset_view_action(request, dataset_id, template, nextstep=None):
         
     elif request.method == 'POST':
         mds.incoming_form(request.POST, dataset_id)
-        # when editing, redirect to dataset view
-        # when creating new, pass to outlier definition
-        if mds.is_new_dataset:
+        # FIXME check for problems in mds:
+        
+        # when editing: redirect to dataset view, when creating new: next step
+        if mds.status == 'creating':
             return redirect('/kantele/dataset/{0}/{1}'.format(nextstep, mds.obj_id)) 
         else:
             return redirect('/kantele/dataset/{0}'.format(mds.obj_id)) 
 
     elif request.method == 'GET':
-        mds.load_from_db(request, dataset_id)
+        mds.load_from_db(request.user, dataset_id)
         return render(request, 'metadata/{0}'.format(template), {'mds': mds} )
     
 
