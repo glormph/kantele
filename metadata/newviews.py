965866fc85f5f2df9f9ff57c1623a9fa9c5b5d29
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2013-01-22 15:54:14 +0100

    Small changes in views

--- metadata/newviews.py
+++ metadata/newviews.py
@@ -11,9 +11,9 @@ empty_mds = metadata.MetadataSet()
 @login_required
 def new_dataset(request):
     mds = copy.deepcopy(empty_mds)
-    oid = mds.initialize_new_dataset()
+    oid = mds.initialize_new_dataset(request)
     # FIXME store oid also in sql
-    redirect('/kantele/files/{0}'.format(str(oid)) )
+    redirect('/kantele/files/{0}'.format(oid))
 
 @login_required
 def edit_dataset(request, dataset_id):
@@ -62,9 +62,13 @@ def dataset_view_action(request, dataset_id, template, nextstep=None):
     elif request.method == 'POST':
         mds.incoming_form(request.POST, dataset_id)
         # FIXME check for problems in mds:
-        
+        # in case of mds.error:
+            # pass mds to get request?
         # when editing: redirect to dataset view, when creating new: next step
         if mds.status == 'creating':
+            # there is a button for more outliers
+            if template == 'outliers.html' and mds.more_outliers == True:
+                nextstep = 'outliers'
             return redirect('/kantele/dataset/{0}/{1}'.format(nextstep, mds.obj_id)) 
         else:
             return redirect('/kantele/dataset/{0}'.format(mds.obj_id)) 
@@ -73,8 +77,3 @@ def dataset_view_action(request, dataset_id, template, nextstep=None):
         mds.load_from_db(request.user, dataset_id)
         return render(request, 'metadata/{0}'.format(template), {'mds': mds} )
     
-
-def test(request, dataset_id):
-    print [x.mongoid for x in Dataset.objects.filter(user=request.user.pk) ]
-    return redirect('/kantele')    
-
