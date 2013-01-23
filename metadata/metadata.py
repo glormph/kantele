d3a2e278ad344b0a8e41527474a9247a0e5cf514
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2013-01-23 13:56:30 +0100

    Wrote metadataset class

---
+++ metadata/metadata.py
@@ -0,0 +1,150 @@
+import os
+from pymongo.objectid import ObjectId
+from parameters import ParameterSet
+from db.dbaccess import DatabaseAccess
+
+class MetadataSet(object):
+    def __init__(self):
+        self.is_new_dataset = False
+        self.obj_id = False
+        self.status = False
+        self.more_outliers = False
+        self.db = DatabaseAccess()
+    
+    def initialize_new_dataset(self, request):
+        # FIXME what to do if the button is pressed twice? Create new or show
+        # the old from session?
+        draft_oid = self.db.insert_draft_metadata( {} )
+        request.session['draft_id'] = draft_oid 
+        self.db.insert_draft_files({'draft_id': draft_oid})
+        self.db.insert_draft_outliers({'draft_id': draft_oid})
+        return str(draft_oid)
+    
+    def create_draft_from_existing_metadata(self):
+        md = {self.fullmeta['metadata'][k] for k in self.fullmeta['metadata']}
+        md['metadata_id'] = str(self.fullmeta['_id'])
+        md['status'] = 'editing'
+        
+        files = {'files': self.fullmeta['files'][:], 
+                'metadata_id': str(self.fullmeta['_id'])}
+        
+        # FIXME When multiple people concurrently edit the metadata, you can't just remove
+        # need an id to bind to a session or something
+        self.mongo.run('remove', 'draftmeta', {'metadata_id': md['metadata_id']})
+        self.mongo.run('remove', 'files', {'metadata_id': md['metadata_id']})
+        self.mongo.run('remove', 'outliers', {'metadata_id': md['metadata_id']})
+
+        self.db.insert_draft_record('draftmeta', md)
+        self.db.insert_draft_record('files', files)
+        
+        for outlier in self.fullmeta['outliers']:
+            outlier['metadata_id'] = str(self.fullmeta['_id'])
+            self.insert_draft_record('outliers', outlier)
+
+    def load_from_db(self, request, obj_id_str, make_paramsets=True):
+        self.obj_id_str = obj_id_str
+        self.obj_id = ObjectId(obj_id_str)
+        self.fullmeta = self.db.get_metadata(self.obj_id)
+        if not self.fullmeta:
+            # data is either draft, or doesn't exist
+            # check if session ID ok
+            if request.session.get('draft_id', None) == self.obj_id:
+                pass # TODO error, NO ACCESS or redirect!
+            # check if exist in draft
+            else:
+                self.basemetadata = self.db.get_metadata(self.obj_id,
+                        draft=True)
+                if not self.basemetadata:
+                    pass # NO ACCESS!, redirect
+
+                else: # load also outliers and files
+                    self.files = self.get_files(self.obj_id)
+                    self.outliers = []
+                    for record in self.get_outliers(self.obj_id):
+                        self.outliers.append(record)
+                     
+        else:
+            self.basemetadata = self.fullmeta['metadata']
+            # TODO extract outliers et al from fullmeta
+
+        if make_paramsets:
+            self.baseparamset = ParameterSet()
+            self.baseparamset.initialize(request.user, self.basemetadata)
+            self.outlierparamsets = []
+            for record in self.outliers: 
+                pset = ParameterSet()
+                pset.initialize(request.user, record)
+                self.outlierparamsets.append( pset )
+
+    def incoming_form(self, req, obj_id_str):
+        obj_id = ObjectId(obj_id_str)
+        # validate session id with req.
+        if not req.session.get('draft_id') == obj_id_str:
+            pass # NO ACCESS!
+        
+        # with obj_id, get tmp metadata['sessionid']
+        # paramset, validate, store.
+        formtgt = [x for x in req.POST if x.startswith('target_')][0]
+        
+        # FIXME This if block isn't correct - add_files 
+        if formtgt == 'target_add_files':
+            if not req.POST['pastefiles'] and \
+                'selectfiles' not in req.POST:
+                pass # FIXME set error flag and redirect to file form
+            else:
+                filelist = req.POST['pastefiles']
+                if filelist == '':
+                    filelist = []
+                else:
+                    filelist = [x.strip() for x in filelist.strip().split('\n')]
+                if 'selectfiles' in req.POST:
+                    filelist.extend(req.POST.getlist('selectfiles'))
+                files = { x : {} for x in filelist }
+                files['draft_id'] = obj_id
+                self.db.insert_files(files)
+
+        elif formtgt in ['target_write_metadata','target_define_outliers',
+                        'more_outliers']:
+            self.paramset = ParameterSet()
+            self.paramset.incoming_metadata(req.POST)
+
+            if not self.paramset.error:
+                self.paramset.do_autodetection()
+                self.paramset.generate_metadata_for_db()
+                if formtgt == 'target_write_metadata':
+                    # TODO check if we need to write more than just
+                    # paramset.metadata here. _id stays the same even w/o $set
+                    self.db.update_draft_metadata(obj_id,
+                            self.paramset.metadata)
+                else:
+                    outlierfiles = req.POST.get('outlierfiles', None)
+                    # check outliers
+                    if not outlierfiles:
+                        pass # ERROR, no files specified
+                    self.load_from_db(req, obj_id_str, make_paramsets=False)
+                    if self.paramset.metadata == self.basemetadata:
+                        pass # ERROR, basemeta == outlier
+                    
+                    for outlier in self.outliers:
+                        if self.paramset.metadata == outlier['metadata']:
+                            pass # ERROR, outlier == previous outlier
+                        elif set(outlierfiles).intersection(outlier['files']):
+                            pass # ERROR
+
+                    # Checks passed, save to db:
+                    self.more_outliers = 'more_outliers'==formtgt
+                    meta_for_db = { 'draft_id': obj_id_str, 
+                                    'metadata': self.paramset.metadata,
+                                    'files': outlierfiles }
+
+                    self.db.insert_outliers(meta_for_db)
+
+            else:
+                pass
+                # TODO check for errors, set flag and redirect target
+        
+    
+
+    def get_uploaded_files(self):
+        return sorted(os.listdir('/mnt/kalevalatmp'))
+
