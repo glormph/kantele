308348bf04a632ae3aea1f8702409cb7c67835d5
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2013-01-28 17:03:52 +0100

    Added models to metadata

--- metadata/models.py
+++ metadata/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Dataset(models.Model):
+    user = models.ForeignKey(User)
+    mongoid = models.CharField(max_length=100)
+    date = models.DateField('date created')
+    project = models.CharField(max_length=100)
+    experiment = models.CharField(max_length=100)
+
+    
+class Template(models.Model):
+    user = models.ForeignKey(User)
+    mongoid = models.CharField(max_length=100)
+    name = models.CharField(max_length=100) 
+    date = models.DateField()
+
+
