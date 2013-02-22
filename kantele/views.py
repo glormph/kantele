59b90dbc4b36382ce1a34b79b51ba910ee14682f
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2013-02-22 14:25:19 +0100

    Showing news and server status on home page

--- kantele/views.py
+++ kantele/views.py
@@ -2,21 +2,25 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from metadata.models import Dataset
-import json
+from news.models import Entry
+import json, os
+import consts
 
 def home(request, message=None):
     # This should be via a class interface to Vainamoinen if specified
     try:
-        with open('infofile_status.json') as fp:
+        with open(os.path.join('/mnt','kalevalatmp', 'infofile_status.json')) as fp:
             status = json.load(fp)
     except:
         pass # FIXME logging
 
+    news = Entry.objects.all().order_by('-date')[:5]
+    
     if request.user.is_authenticated():
         datasets = Dataset.objects.filter(datasetowner__owner=request.user).order_by('-date')[:5]
     else:
         datasets = None
-    return render(request, 'kantele/index.html', {'status': status, 
+    return render(request, 'kantele/index.html', {'news': news, 'status': status, 
         'datasets': datasets} )
 
 @login_required
