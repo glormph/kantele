b5c5b44305962aa16810877b8519363313683dcd
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2016-01-12 22:20:08 +0100

    Removed unnecessary outlier stuff from metadata flow

--- kantele/urls.py
+++ kantele/urls.py
@@ -1,29 +1,35 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
 admin.autodiscover()
- 
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^kantele/login/$', 'django.contrib.auth.views.login'),
     url(r'^kantele/logout/$', 'kantele.views.logout_page'),
-    
+
     url(r'^kantele/$', 'kantele.views.home'),
     url(r'^kantele/userdatasets/$', 'kantele.views.all_user_datasets'),
-    
+
 
     url(r'^kantele/dataset/new/$', 'metadata.views.new_dataset'),
-    url(r'^kantele/dataset/edit/(?P<dataset_id>\w+)/$', 'metadata.views.edit_dataset'),
-    url(r'^kantele/dataset/copy/(?P<dataset_id>\w+)/$', 'metadata.views.copy_dataset'),
+    url(r'^kantele/dataset/edit/(?P<dataset_id>\w+)/$',
+        'metadata.views.edit_dataset'),
+    url(r'^kantele/dataset/copy/(?P<dataset_id>\w+)/$',
+        'metadata.views.copy_dataset'),
 
-    url(r'^kantele/dataset/files/(?P<dataset_id>\w+)/$', 'metadata.views.select_files'),
-    url(r'^kantele/dataset/metadata/(?P<dataset_id>\w+)/$', 'metadata.views.write_metadata'),
-    url(r'^kantele/dataset/outliers/(?P<dataset_id>\w+)/$', 'metadata.views.define_outliers'),
-    url(r'^kantele/dataset/store/(?P<dataset_id>\w+)/$', 'metadata.views.store_dataset'),
-    
-    url(r'^kantele/dataset/(?P<dataset_id>\w+)/$', 'metadata.views.show_dataset'),
+    url(r'^kantele/dataset/files/(?P<dataset_id>\w+)/$',
+        'metadata.views.select_files'),
+    url(r'^kantele/dataset/metadata/(?P<dataset_id>\w+)/$',
+        'metadata.views.write_metadata'),
+    url(r'^kantele/dataset/store/(?P<dataset_id>\w+)/$',
+        'metadata.views.store_dataset'),
+
+    url(r'^kantele/dataset/(?P<dataset_id>\w+)/$',
+        'metadata.views.show_dataset'),
 
     url(r'^kantele/rawstatus/$', 'rawstatus.views.raw_file_processed'),
 
@@ -32,4 +38,3 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
-
--- metadata/views.py
+++ metadata/views.py
@@ -4,15 +4,17 @@ from django.contrib.auth.decorators import login_required
 from models import Dataset, DraftDataset
 from db import dbaccess
 import copy
-import metadata 
+import metadata
 
 empty_mds = metadata.MetadataSet()
+
 
 @login_required
 def new_dataset(request):
     mds = deepcopy_metadataset(empty_mds)
     oid = mds.new_dataset(request)
     return redirect('/kantele/dataset/files/{0}'.format(oid))
+
 
 @login_required
 def edit_dataset(request, dataset_id):
@@ -27,6 +29,7 @@ def copy_dataset(request, dataset_id):
     oid = mds.copy_dataset(request, dataset_id)
     return redirect('/kantele/dataset/files/{0}'.format(oid))
 
+
 @login_required
 def show_dataset(request, dataset_id):
     if request.method == 'GET':
@@ -34,17 +37,17 @@ def show_dataset(request, dataset_id):
     else:
         return redirect('/kantele')
 
+
 @login_required
-def select_files(request, dataset_id):    
-    return dataset_view_action(request, dataset_id, 'file_input.html', 'metadata')
+def select_files(request, dataset_id):
+    return dataset_view_action(request, dataset_id, 'file_input.html',
+                               'metadata')
+
 
 @login_required
 def write_metadata(request, dataset_id):
-    return dataset_view_action(request, dataset_id, 'base_meta.html', 'outliers')
+    return dataset_view_action(request, dataset_id, 'base_meta.html', 'store')
 
-@login_required
-def define_outliers(request, dataset_id):
-    return dataset_view_action(request, dataset_id, 'outliers.html', 'store')
 
 @login_required
 def store_dataset(request, dataset_id):
@@ -56,49 +59,50 @@ def store_dataset(request, dataset_id):
         if not mds:
             return redirect('/kantele')
         else:
-            mds.store_dataset(request, dataset_id) 
-            return redirect('/kantele') # Can we add a message: congratulations here?
+            mds.store_dataset(request, dataset_id)
+            return redirect('/kantele')  # Can we add a message here?
+
+
+def post_dataset(mds, request, dataset_id, template, nextstep=None):
+    mds.incoming_form(request, dataset_id)
+    if mds.error:
+        if mds.error['redirect'] == 'return_to_form':
+            mds.show_errored_dataset(request, dataset_id)
+            return return_get(request, template, dataset_id, mds,
+                              message=mds.error['message'])
+        elif mds.error['redirect'] == 'home':
+            return redirect('home', message=mds.error['message'])
+    # when editing: redirect to dataset view, when creating new: next step
+    if request.session.get('metadatastatus', None) == 'new':
+        return redirect('/kantele/dataset/{0}/{1}'.format(nextstep,
+                                                          dataset_id))
+    else:
+        return redirect('/kantele/dataset/{0}'.format(mds.obj_id))
+
+
+def return_get(request, template, ds_id, mds, message=None):
+    return render(request, 'metadata/{0}'.format(template),
+                  {'mds': mds, 'ds_id': ds_id, 'message': message})
 
 
 def dataset_view_action(request, dataset_id, template, nextstep=None):
-    def return_get(request, template, ds_id, mds, message=None):
-        return render(request, 'metadata/{0}'.format(template), {'mds': mds,
-                'ds_id': ds_id, 'message': message} )
-
     mds = check_dataset_owner(request, dataset_id)
     if not mds:
         return redirect('/kantele')
-        
     elif request.method == 'POST':
-        mds.incoming_form(request, dataset_id)
-        if mds.error:
-            if mds.error['redirect'] == 'return_to_form':
-                mds.show_errored_dataset(request, dataset_id)
-                return return_get(request, template, dataset_id, mds,
-                        message=mds.error['message'])
-            elif mds.error['redirect'] == 'home':
-                return redirect('home', message=mds.error['message'])
-
-        # when editing: redirect to dataset view, when creating new: next step
-        if request.session.get('metadatastatus', None) == 'new':
-            # there is a button for more outliers
-            if template == 'outliers.html' and mds.more_outliers == True:
-                nextstep = 'outliers'
-            return redirect('/kantele/dataset/{0}/{1}'.format(nextstep,
-                    dataset_id)) 
-        else:
-            return redirect('/kantele/dataset/{0}'.format(mds.obj_id)) 
-
+        post_dataset(mds, request, dataset_id, template, nextstep)
     elif request.method == 'GET':
         mds.show_dataset(request, dataset_id)
         return return_get(request, template, dataset_id, mds)
-    
+
+
 def deepcopy_metadataset(mds):
     """Database access cannot be deepcopied since it contains mongo
     collections"""
     new_mds = copy.deepcopy(mds)
     new_mds.db = dbaccess.DatabaseAccess()
     return new_mds
+
 
 def check_dataset_owner(request, dataset_id):
     if request.method not in ['POST', 'GET']:
@@ -107,7 +111,8 @@ def check_dataset_owner(request, dataset_id):
             [x.mongoid for x in Dataset.objects.filter(user=request.user.pk)]:
         return deepcopy_metadataset(empty_mds)
     elif dataset_id in \
-            [x.mongoid for x in DraftDataset.objects.filter(user=request.user.pk)]:
+            [x.mongoid for x in
+             DraftDataset.objects.filter(user=request.user.pk)]:
         return deepcopy_metadataset(empty_mds)
     else:
         return False
