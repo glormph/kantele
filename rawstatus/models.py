809ef1f5f8c65d290d17217dbdad1438cd7a4972
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-07-09 10:24:19 +0200

    Updated rawstatus models so they become correct

--- rawstatus/models.py
+++ rawstatus/models.py
@@ -3,18 +3,30 @@ from django.db import models
 
 class Producer(models.Model):
     name = models.CharField(max_length=100)
-    client_id = models.CharField()  # for registration, UUID or perhaps auth
-    heartbeat = models.DateTimeField('last seen')
+    client_id = models.CharField(max_length=100)
+    heartbeat = models.DateTimeField('last seen', auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class RawFile(models.Model):
-    name = models.CharField()
+    """Data (raw) files as reported by instrument"""
+    name = models.CharField(max_length=100)
     producer = models.ForeignKey(Producer)
-    source_md5 = models.CharField()
+    source_md5 = models.CharField(max_length=32, unique=True)
     size = models.IntegerField('size in bytes')
     date = models.DateTimeField('date created')
 
+    def __str__(self):
+        return self.name
+
 
 class TransferredFile(models.Model):
-    fileid = models.ForeignKey(RawFile)
-    md5 = models.CharField()
+    """Files transferred from instrument to storage"""
+    rawfile = models.ForeignKey(RawFile)
+    fnpath = models.CharField(max_length=200)
+    md5 = models.CharField(max_length=32, blank=True)
+
+    def __str__(self):
+        return self.rawfile.name
