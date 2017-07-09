240291b51f15f6bbae1a2bd0e2b6a26e30bdf5d5
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-07-09 10:41:33 +0200

    New settings and modules for updating django version

---
+++ rawstatus/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from rawstatus.views import Producer
+
+# Register your models here.
+
+admin.site.register(Producer)
---
+++ rawstatus/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RawstatusConfig(AppConfig):
+    name = 'rawstatus'
