a1570fb4545a253fab6e986c4545d54a86080aa9
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-07-09 10:25:36 +0200

    Views rawstatus now work, files are saved. md5 is mock-checked

--- rawstatus/views.py
+++ rawstatus/views.py
@@ -1,18 +1,13 @@
+from django.db.utils import IntegrityError
 from django.http import (JsonResponse, HttpResponseForbidden,
                          HttpResponseNotAllowed)
-from filetracker import (RawFile, Producer, TransferredFile)
-
-# FLow is like this:
-# POST fn/md5/instrument/aqcuis.date
-    # response JSON, with tmpfn or datedir, as to not overwrite
-    # (client SCPs, if success)
-# POST scp complete
-    # response JSON
-    # check MD5 in background
-# GET is-md5-ok-of-fn-with-id?
-    # response JSON with true or false
+from django.core.exceptions import ObjectDoesNotExist
+from rawstatus.models import (RawFile, Producer, TransferredFile)
+from rawstatus import tasks
+from datetime import datetime
 
 
+# Create your views here.
 def check_producer(producer_id):
     return Producer.objects.get(client_id=producer_id)
 
@@ -26,16 +21,39 @@ def register_file(request):
     """
     if request.method == 'POST':
         try:
-            producer = check_producer(request.POST.client_id)
-        except Producer.DoesNotExist:
+            client_id = request.POST['client_id']
+            fn = request.POST['fn']
+            size = request.POST['size']
+            md5 = request.POST['md5']
+            filedate_raw = request.POST['date']
+            file_date = datetime.strptime(filedate_raw, '%Y%m%d.%H:%M')
+        except KeyError as error:
+            print('POST request to register_file with missing parameter, '
+                  '{}'.format(error))
             return HttpResponseForbidden()
-        file_record = RawFile(request.fn, producer, request.md5, request.size,
-                              request.date)
-        # FIXME check if not already exist!
-        file_record.save()
+        try:
+            producer = check_producer(client_id)
+        except Producer.DoesNotExist:
+            print('POST request with incorrect client id '
+                  '{}'.format(request.POST['client_id']))
+            return HttpResponseForbidden()
+        except ValueError as error:
+            print('POST request to register_file with incorrect formatted '
+                  'date parameter {}'.format(error))
+            return HttpResponseForbidden()
+        file_record = RawFile(name=fn, producer=producer, source_md5=md5,
+                              size=size, date=file_date)
+        try:
+            file_record.save()
+        except IntegrityError:
+            # FIXME make a response with existing id and state?
+            existing_fn = RawFile.objects.get(source_md5=md5)
+            print('File already exists', existing_fn)
+            return HttpResponseForbidden()
+            #return JsonResponse(
         return JsonResponse({'file_id': file_record.id, 'state': 'registered'})
     else:
-        return HttpResponseNotAllowed()
+        return HttpResponseNotAllowed(permitted_methods=['POST'])
 
 
 def file_transferred(request):
@@ -43,23 +61,32 @@ def file_transferred(request):
         - fn_id
     Starts checking file MD5 in background
     """
-    fn_id = request.POST.fn_id
     if request.method == 'POST':
         try:
-            check_producer(request.POST.client_id)
+            fn_id = request.POST['fn_id']
+            client_id = request.POST['client_id']
+        except KeyError as error:
+            print('POST request to register_file with missing parameter, '
+                  '{}'.format(error))
+            return HttpResponseForbidden()
+        try:
+            check_producer(client_id)
         except Producer.DoesNotExist:
             return HttpResponseForbidden()
         try:
-            file_transferred = TransferredFile.objects.get(fileid=fn_id)
-        except TransferredFile.DoesNotExist:
-            file_transferred = TransferredFile(fn_id)
+            file_transferred = TransferredFile.objects.get(rawfile_id=fn_id)
+        except ObjectDoesNotExist:
+            # FIXME fnpath
+            print('New transfer registered, fn_id {}'.format(fn_id))
+            file_transferred = TransferredFile(rawfile_id=fn_id)
             file_transferred.save()
-            # FIXME start background process for MD5
+            # FIXME start background process for MD5, unimplemented, call w
+            # delay,
+            tasks.get_md5(file_transferred.rawfile.id)
             return JsonResponse({'fn_id': request.POST.fn_id,
                                  'md5_state': False})
-        # FIXME rawfile has no file_id field in model, how?
-        file_registered = RawFile(file_id=fn_id)
-        # FIXME default value at not saved (NULL in db)? FAlse or None?
+        print('Transfer state requested for fn_id {}'.format(fn_id))
+        file_registered = file_transferred.rawfile
         if not file_transferred.md5:
             return JsonResponse({'fn_id': fn_id, 'md5_state': False})
         if file_registered.source_md5 == file_transferred.md5:
