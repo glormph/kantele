30851756020e241dff082dc930d66cbbe5509eef
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-07-14 11:32:50 +0200

    New models for datasets

--- metadata/models.py
+++ metadata/models.py
@@ -1,23 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rawstatus.models import RawFile
 
 
 class Dataset(models.Model):
     user = models.ForeignKey(User)
-    mongoid = models.CharField(max_length=100)
     date = models.DateTimeField('date created')
     project = models.CharField(max_length=100)
     experiment = models.CharField(max_length=100)
 
 
-class DatasetOwner(models.Model):
+class DatasetRawFile(models.Model):
     dataset = models.ForeignKey(Dataset)
-    owner = models.ForeignKey(User)
-   
+    rawfile = models.ForeignKey(RawFile)
+
+
+class DatasetPI(models.Model):
+    dataset = models.ForeignKey(Dataset)
+    contact_email = models.CharField(max_length=100)
+
+
+class DatasetContact(models.Model):
+    """Exteral contacts if not PI can be entered in here"""
+    dataset = models.ForeignKey(Dataset)
+    contact_email = models.CharField(max_length=100)
+
 
 class DraftDataset(models.Model):
     user = models.ForeignKey(User)
     mongoid = models.CharField(max_length=100)
     date = models.DateField('date created')
-
-
