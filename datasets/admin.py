ec7f9143f41177ae9f56288b4d394519ea811088
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-08-21 09:45:31 +0200

    Added adminable models

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 
-from datasets.models import HiriefRange, Datatype, PrincipalInvestigator
+from datasets.models import (HiriefRange, Datatype, PrincipalInvestigator,
+                             SelectParameter,
+                             SelectParameterOption,
+                             FieldParameter,
+                             Enzyme, QuantType, QuantChannel, QuantTypeChannel,
+                             Operator,
+                             )
 
 # Register your models here.
 
+admin.site.register(FieldParameter)
+admin.site.register(SelectParameter)
+admin.site.register(SelectParameterOption)
 admin.site.register(HiriefRange)
 admin.site.register(Datatype)
 admin.site.register(PrincipalInvestigator)
+admin.site.register(Enzyme)
+admin.site.register(QuantType)
+admin.site.register(QuantChannel)
+admin.site.register(QuantTypeChannel)
+admin.site.register(Operator)
