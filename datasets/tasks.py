c25e17c7fd07528e94d15232f20a0b99b700aa13
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-10-05 15:06:39 +0200

    Exception handling and prints for dataset tasks

--- datasets/tasks.py
+++ datasets/tasks.py
@@ -27,21 +27,25 @@ def move_file_storage(self, fn, srcshare, srcpath, dstpath, fn_id):
         print(msg)
         shutil.move(dst, src)
         raise RuntimeError(msg)
+    print('File {} moved to {}'.format(fn_id, dst))
 
 
 @shared_task(bind=True, queue=config.QUEUE_STORAGE)
 def move_stored_file_tmp(self, fn, path, fn_id):
     src = os.path.join(config.STORAGESHARE, path, fn)
     dst = os.path.join(config.TMPSHARE, fn)
-    print('Moving stored file {} to tmp'.format(src))
+    print('Moving stored file {} to tmp'.format(fn_id))
     shutil.move(src, dst)
     # FIXME API call to add new path to db, MV BACK IF HTTP NOT 200
     postdata = {'fn_id': fn_id, 'servershare': config.TMPSHARENAME,
                 'dst_path': ''}
     url = urljoin(config.KANTELEHOST, reverse('rawstatus-updatestorage'))
-    req = requests.post(url=url, data=postdata)
-    if not req.status_code == 200:
+    try:
+        req = requests.post(url=url, data=postdata)
+    except (requests.exceptions.HTTPError,
+            requests.exceptions.ConnectionError):
         msg = 'Could not update database: http {}'.format(req.status_code)
         print(msg)
         shutil.move(dst, src)
         raise RuntimeError(msg)
+    print('File {}  moved to tmp'.format(fn_id))
