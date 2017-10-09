0ad416a9f0928e2743339915d2a279382b647139
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-10-09 11:08:11 +0200

    Atomic requests was in wrong place, did not work

--- kantele/settings.py
+++ kantele/settings.py
@@ -68,12 +68,12 @@ WSGI_APPLICATION = 'kantele.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-ATOMIC_REQUESTS = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ATOMIC_REQUESTS': True,
     }
 }
 
