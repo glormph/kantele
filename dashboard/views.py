22716b29b34ae8b8358c5fe8fce94b61e519f1b5
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-11-27 21:43:55 +0100

    Add success tasks and color job state

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,9 +9,9 @@ def show_jobs(request):
     for task in Task.objects.filter(
             job__state__in=[Jobstates.PENDING, Jobstates.PROCESSING,
                         Jobstates.ERROR]):
-        freshjob = {'name': task.job.funcname, 'user': 'jorrit',
+        freshjob = {'name': task.job.funcname, 'user': 'TBD',
                     'date': task.job.timestamp,
-                    'tasks': {'PENDING': 0, 'FAILURE': 0}}
+                    'tasks': {'PENDING': 0, 'FAILURE': 0, 'SUCCESS': 0}}
         if not task.job.state in context['jobs']:
             context['jobs'][task.job.state] = {task.job.id: freshjob}
         elif not task.job.id in context['jobs'][task.job.state]:
