4d9ac05542eb4efd1943655a43d24104fab8e9d8
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-11-28 12:09:18 +0100

    Dashboard is now a vue page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from dashboard import views
 
 app_name = 'dash'
 urlpatterns = [
+    url(r'^$', views.dashboard, name='dash'),
     url(r'^jobs/$', views.show_jobs, name='jobs'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,23 +1,31 @@
-from django.shortcuts import render
+from datetime import datetime
 
-from jobs.jobs import Jobstates
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+
+from jobs.jobs import Jobstates, is_job_retryable
 from jobs.models import Task
 
 
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
+
+
 def show_jobs(request):
-    context = {'jobs': {}}
+    jobs = {}
     for task in Task.objects.filter(
             job__state__in=[Jobstates.PENDING, Jobstates.PROCESSING,
                         Jobstates.ERROR]):
         freshjob = {'name': task.job.funcname, 'user': 'TBD',
-                    'date': task.job.timestamp,
+                    'date': datetime.strftime(task.job.timestamp, '%Y%m%d'),
+                    'retry': is_job_retryable(task.job),
                     'tasks': {'PENDING': 0, 'FAILURE': 0, 'SUCCESS': 0}}
-        if not task.job.state in context['jobs']:
-            context['jobs'][task.job.state] = {task.job.id: freshjob}
-        elif not task.job.id in context['jobs'][task.job.state]:
-            context['jobs'][task.job.state][task.job.id] = freshjob
-        context['jobs'][task.job.state][task.job.id]['tasks'][task.state] += 1
+        if not task.job.state in jobs:
+            jobs[task.job.state] = {task.job.id: freshjob}
+        elif not task.job.id in jobs[task.job.state]:
+            jobs[task.job.state][task.job.id] = freshjob
+        jobs[task.job.state][task.job.id]['tasks'][task.state] += 1
     for jstate in [Jobstates.PENDING, Jobstates.PROCESSING, Jobstates.ERROR]:
-        if jstate in context['jobs']:
-            context['jobs'][jstate] = context['jobs'][jstate].values()
-    return render(request, 'dashboard/dashboard.html', context)
+        if jstate in jobs:
+            jobs[jstate] = [x for x in jobs[jstate].values()]
+    return JsonResponse(jobs)
