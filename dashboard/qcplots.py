5114e7c240376ac5d248956b180c9c4389773844
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-12-14 12:38:09 +0100

    Fixes showing plots from DB and storing them

--- dashboard/qcplots.py
+++ dashboard/qcplots.py
@@ -9,73 +9,52 @@ from bokeh.models import Legend
 
 def boxplotrange(qcdata, qckey, instruments):
     width = timedelta(7)
-    #plot.title = 'Peptide precursor area'
     plots = []
     for instrument in instruments:
+        if instrument not in qcdata:
+            continue
         plot = figure(x_axis_type='datetime', height=200, width=300,
                       title=instrument, toolbar_location='above')
-        for qc in qcdata:
-            date = qc['date']
-            data = qc[instrument][qckey]
+        for date, data in qcdata[instrument][qckey].items():
             boxplot(plot, date, data, width, 'blue')
         plots.append(plot)
     return row(*plots)
 
 
-def missedcleavageplot(qcdata, instruments):
-    plots, remodeled = [], {}
-    for dateddata in qcdata:
-        date = dateddata['date']
-        for instrument in instruments:
-            if instrument not in dateddata:
-                continue
-            data = dateddata[instrument]['miscleav']
-            print(data)
-            for key, val in data.items():
-                try:
-                    remodeled[key][instrument].append((date, val))
-                except KeyError:
-                    try:
-                        remodeled[key][instrument] = [(date, val)]
-                    except KeyError:
-                        remodeled[key] = {instrument: [(date, val)]}
-    # {'3': {'luke': [(20170101, 3), (20170501, 4)], 'leia': ...
-    colors = cycle(d3['Category10'][10])
-    plot = figure(x_axis_type='datetime', width=350, height=200,
-                  title=instruments[0], toolbar_location='above')
-    leglines = []
-    for key, color in zip(sorted(remodeled.keys()), colors):
-        leglines.append(plot.line([x[0] for x in remodeled[key][instruments[0]]],
-                                  [x[1] for x in remodeled[key][instruments[0]]],
-                                  color=color))
-    legend = Legend(items=[(key, [line]) for
-                           key, line in zip(remodeled, leglines)])
-    plots.append(plot)
-    plot.add_layout(legend, 'left')
-    for instrument in instruments[1:]:
-        plot = figure(x_axis_type='datetime', width=300, height=200,
-                      title=instrument, toolbar_location='above')
-        for key, color in zip(remodeled, colors):
-            plot.line([x[0] for x in remodeled[key][instrument]],
-                      [x[1] for x in remodeled[key][instrument]], color=color)
-        plots.append(plot)
-    return row(*plots)
-
-
 def boxplot(plot, x, data, width, color='black'):
-    plot.segment(x, data['upper'], x, data['q3'], line_width=2, line_color=color)
-    plot.segment(x, data['lower'], x, data['q1'], line_width=2, line_color=color)
-    plot.rect(x, (data['q3'] + data['q2']) / 2, width, (data['q3'] - data['q2']), line_width=2, line_color='black', fill_color=color)
-    plot.rect(x, (data['q2'] + data['q1']) / 2, width, (data['q2'] - data['q1']), line_width=2, line_color='black', fill_color=color)
+    plot.segment(x, data['upper'], x, data['q3'], line_width=2,
+                 line_color=color)
+    plot.segment(x, data['lower'], x, data['q1'], line_width=2,
+                 line_color=color)
+    plot.rect(x, (data['q3'] + data['q2']) / 2, width,
+              (data['q3'] - data['q2']), line_width=2, line_color='black',
+              fill_color=color)
+    plot.rect(x, (data['q2'] + data['q1']) / 2, width,
+              (data['q2'] - data['q1']), line_width=2, line_color='black',
+              fill_color=color)
 
 
 def timeseries_line(qcdata, key, instruments):
-    plot = figure(x_axis_type='datetime', height=300)
-    leglines = []
-    for instrument, color in zip(instruments, cycle(d3['Category10'][10])):
-        leglines.append(plot.line([x['date'] for x in qcdata],
-                                  [x[instrument][key] for x in qcdata],
-                                  color=color))
-    plot.add_layout(Legend(items=[(inst, [line]) for inst, line in
-                                  zip(instruments, leglines)]), 'right')
-    return plot
+    plots = []
+    firstplot = False
+    legcolors, colors = {}, cycle(d3['Category10'][10])
+    for instrument in instruments:
+        if instrument not in qcdata:
+            continue
+        leglines = {}
+        plot = figure(x_axis_type='datetime', height=200)
+        for category, catdata in qcdata[instrument][key].items():
+            try:
+                col = legcolors[category]
+            except KeyError:
+                col = next(colors)
+                legcolors[category] = col
+            leglines[category] = plot.line([dpoint[0] for dpoint in catdata],
+                                           [dpoint[1] for dpoint in catdata],
+                                           color=col)
+        if not firstplot:
+            firstplot = plot
+        plots.append(plot)
+    firstplot.add_layout(Legend(items=[(cat, [line]) for cat, line in
+                                       sorted(leglines.items())]), 'left')
+    return row(*plots)
--- dashboard/views.py
+++ dashboard/views.py
@@ -23,14 +23,12 @@ def store_longitudinal_qc(request):
     """This method is fed JSON over POST"""
     if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
-        print(data)
         if ('client_id' not in data or
                 data['client_id'] not in settings.CLIENT_APIKEYS):
             return HttpResponseForbidden()
         try:
             qcrun = models.QCData.objects.get(rawfile_id=data['rf_id'])
         except models.QCData.DoesNotExist:
-            # FIXME save data when?
             qcrun = models.QCData(rawfile_id=data['rf_id'],
                                   analysis_id=data['analysis_id'])
             qcrun.save()
@@ -47,46 +45,100 @@ def store_longitudinal_qc(request):
 
 def create_newplot(qcrun, qcdata, plot_id):
     if 'q1' in qcdata:
-        entry = models.BoxplotData(plot_id=plot_id, qcrun=qcrun,
-                                   upper=qcdata['upper'],
-                                   lower=qcdata['lower'],
-                                   q1=qcdata['q1'], q2=qcdata['q2'],
-                                   q3=qcdata['q3'])
+        models.BoxplotData.objects.create(plot_id=plot_id, qcrun=qcrun,
+                                          upper=qcdata['upper'],
+                                          lower=qcdata['lower'],
+                                          q1=qcdata['q1'], q2=qcdata['q2'],
+                                          q3=qcdata['q3'])
     else:
         for cat, val in qcdata.items():
-            entry = models.LineplotData(plot_id=plot_id, qcrun=qcrun,
-                                        value=val, category=cat)
-    entry.save()
+            models.LineplotData.objects.create(plot_id=plot_id, qcrun=qcrun,
+                                               value=val, category=cat)
 
 
 def update_qcdata(qcrun, data):
-        # FIXME rerun old data on new plots at qc task update --> what happens?
-        plotmap = {p.shortname: p.id for p in models.Plot.objects.all()}
-        old_plots = {p.plot.shortname: p for p in
-                     qcrun.boxplotdata_set.all().select_related('plot')}
-        for lpd in qcrun.lineplotdata_set.all().select_related('plot'):
-            if lpd.plot.shortname not in old_plots:
-                old_plots[lpd.plot.shortname] = [lpd]
+    # FIXME rerun old data on new plots at qc task update --> what happens?
+    plotmap = {p.shortname: p.id for p in models.Plot.objects.all()}
+    old_plots = {p.plot.shortname: p for p in
+                 qcrun.boxplotdata_set.all().select_related('plot')}
+    for lpd in qcrun.lineplotdata_set.all().select_related('plot'):
+        if lpd.plot.shortname not in old_plots:
+            old_plots[lpd.plot.shortname] = [lpd]
+        else:
+            old_plots[lpd.plot.shortname].append(lpd)
+    for plotname, qcdata in data['plots'].items():
+        plot_id = plotmap[plotname]
+        try:
+            oldp = old_plots[plotname]
+        except KeyError:
+            create_newplot(qcrun, qcdata, plot_id)
+        else:
+            if 'q1' in qcdata:
+                oldp.upper = qcdata['upper']
+                oldp.lower = qcdata['lower']
+                oldp.q1 = qcdata['q1']
+                oldp.q2 = qcdata['q2']
+                oldp.q3 = qcdata['q3']
+                oldp.save()
             else:
-                old_plots[lpd.plot.shortname].append(lpd)
-        for plotname, qcdata in data['plots'].items():
-            plot_id = plotmap[plotname]
+                for op in oldp:
+                    op.value = qcdata[op.category]
+                    op.save()
+
+
+def get_longitud_qcdata():
+    long_qc = {}
+    for qcrun in models.QCData.objects.select_related('rawfile__producer'):
+        date, instru = qcrun.rawfile.date, qcrun.rawfile.producer.name
+        if instru not in long_qc:
+            long_qc[instru] = {}
+        for lplot in qcrun.lineplotdata_set.all():
             try:
-                oldp = old_plots[plotname]
+                long_qc[instru][lplot.plot.shortname][lplot.category].append(
+                    (date, lplot.value))
             except KeyError:
-                create_newplot(qcrun, qcdata, plot_id)
-            else:
-                if 'q1' in qcdata:
-                    oldp.upper = qcdata['upper']
-                    oldp.lower = qcdata['lower']
-                    oldp.q1 = qcdata['q1']
-                    oldp.q2 = qcdata['q2']
-                    oldp.q3 = qcdata['q3']
-                    oldp.save()
-                else:
-                    for op in oldp:
-                        op.value = qcdata[op.category]
-                        op.save()
+                try:
+                    long_qc[instru][lplot.plot.shortname][lplot.category] = [
+                        (date, lplot.value)]
+                except KeyError:
+                    long_qc[instru][lplot.plot.shortname] = {
+                        lplot.category: [(date, lplot.value)]}
+        for boxplot in qcrun.boxplotdata_set.all():
+            bplot = {'q1': boxplot.q1, 'q2': boxplot.q2, 'q3': boxplot.q3,
+                     'upper': boxplot.upper, 'lower': boxplot.lower}
+            try:
+                long_qc[instru][boxplot.plot.shortname][date] = bplot
+            except KeyError:
+                long_qc[instru][boxplot.plot.shortname] = {date: bplot}
+    return long_qc
+
+
+def show_qc(request):
+    print('It is run')
+    """
+    QC data:
+        Date, Instrument, RawFile, AnalysisResult
+    """
+    instruments = [x.name for x in Producer.objects.all()]
+    dateddata = get_longitud_qcdata()
+    plot = {
+        'amount_peptides': qcplots.timeseries_line(dateddata, 'nr_peptides',
+                                                   instruments),
+        'amount_proteins': qcplots.timeseries_line(dateddata, 'nr_proteins',
+                                                   instruments),
+        'amount_psms': qcplots.timeseries_line(dateddata, 'nr_psms',
+                                               instruments),
+        'missed_cleav': qcplots.timeseries_line(dateddata, 'miscleav',
+                                                instruments),
+        'precursorarea': qcplots.boxplotrange(dateddata, 'peparea',
+                                              instruments),
+        'prec_error': qcplots.boxplotrange(dateddata, 'perror', instruments),
+        'msgfscore': qcplots.boxplotrange(dateddata, 'msgfscore', instruments),
+        'rt': qcplots.boxplotrange(dateddata, 'rt', instruments),
+            }
+    script, div = components(plot, wrap_script=False, wrap_plot_info=False)
+    print(JsonResponse({'bokeh_code': {'script': script, 'div': div}}))
+    return JsonResponse({'bokeh_code': {'script': script, 'div': div}})
 
 
 def show_jobs(request):
