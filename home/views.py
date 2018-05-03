a22b568a2b543a9c91417e9574a9b0d32f11c4cd
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2018-05-03 18:16:11 +0200

    Changes in home screen, table can expand now

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from datasets import models as dsmodels
 from datasets import jobs as dsjobs
 from rawstatus import models as filemodels
 from jobs import jobs
+from jobs import models as jm
 
 
 @login_required
@@ -18,6 +19,7 @@ def home(request):
 
 @login_required
 def show_datasets(request):
+    # FIXME this should be called with ds_id or a search query
     if 'dsets' in request.GET:
         dsets = [int(x) for x in request.GET['dsets'].split(',')]
         return get_multidset_info(dsets)
@@ -25,77 +27,77 @@ def show_datasets(request):
         'components': [x.name for x in dsmodels.DatasetComponent.objects.all()]
     }
     dsets = OrderedDict()
-    for dcst in dsmodels.DatasetComponentState.objects.select_related(
-            'dataset__runname__experiment__project', 'dtcomp__component',
-            'dataset__prefractionationdataset'):
+    jobs = {}
+    for entry in jm.Job.objects.filter(filejob__storedfile__rawfile__datasetrawfile__dataset_id__in=dsets):
         try:
-            dsets[dcst.dataset.id][dcst.dtcomp.component.name] = dcst.state
+            jobs[job.filejob.storedfile.rawfile.datasetrawfile.dataset_id].add(job.state)
         except KeyError:
-            dsets[dcst.dataset.id] = {
-                'id': dcst.dataset.id,
-                'own': dcst.dataset.user_id == request.user.id,
-                'usr': dcst.dataset.user.username,
-                'proj': dcst.dataset.runname.experiment.project.name,
-                'exp': dcst.dataset.runname.experiment.name,
-                'run': dcst.dataset.runname.name,
-                'dtype': dcst.dataset.datatype.name,
-                'jobs': [],
-                'selected': False,
-                dcst.dtcomp.component.name: dcst.state,
-            }
-            if hasattr(dcst.dataset, 'prefractionationdataset'):
-                pf = dcst.dataset.prefractionationdataset
-                dsets[dcst.dataset.id]['prefrac'] = str(pf.prefractionation.name)
-                if 'hirief' in pf.prefractionation.name.lower():
-                    dsets[dcst.dataset.id]['hr'] = '{} {}'.format('HiRIEF', str(pf.hiriefdataset.hirief))
-    for jfn in filemodels.FileJob.objects.select_related('job', 'storedfile__rawfile__datasetrawfile').exclude(job__state=jobs.Jobstates.DONE).distinct('storedfile__rawfile__datasetrawfile__dataset', 'job'):
-        try:
-            dsets[jfn.storedfile.rawfile.datasetrawfile.dataset_id]['jobs'].append({'name': jfn.job.funcname, 'state': jfn.job.state})
-        except dsmodels.DatasetRawFile.DoesNotExist:
-            pass
+            jobs[job.filejob.storedfile.rawfile.datasetrawfile.dataset_id] = {job.state}
+    for dataset in dsmodels.Dataset.objects.select_related(
+            'runname__experiment__project', 'prefractionationdataset'):
+        jobstates = list(jobs[dataset.id]) if dataset.id in jobs else []
+        dsets[dataset.id] = {
+            'id': dataset.id,
+            'own': dataset.user_id == request.user.id,
+            'usr': dataset.user.username,
+            'proj': dataset.runname.experiment.project.name,
+            'exp': dataset.runname.experiment.name,
+            'run': dataset.runname.name,
+            'dtype': dataset.datatype.name,
+            'is_corefac': dataset.runname.experiment.project.corefac,
+            'jobstates': jobstates,
+            'details': False,
+            'selected': False,
+        }
+        if hasattr(dataset, 'prefractionationdataset'):
+            pf = dataset.prefractionationdataset
+            dsets[dataset.id]['prefrac'] = str(pf.prefractionation.name)
+            if 'hirief' in pf.prefractionation.name.lower():
+                dsets[dataset.id]['hr'] = '{} {}'.format('HiRIEF', str(pf.hiriefdataset.hirief))
     response['dsets'] = dsets
     return JsonResponse(response)
 
+# Ds page links to projects/files/analyses
+# Project page links to files/analyses/datasets
+# Etc CANNOT dynamically code the table too much
+# Make three tables and make them share some code but not all
 
 @login_required
 def get_dset_info(request, dataset_id):
-    info = {'num_dsets': 1}
+    info = {}
     nonms_dtypes = {x.id: x.name for x in dsmodels.Datatype.objects.all()
                     if x.name in ['microscopy']}
-    files = filemodels.StoredFile.objects.filter(
-        rawfile__datasetrawfile__dataset_id=dataset_id)
     dset = dsmodels.Dataset.objects.filter(pk=dataset_id).select_related(
-        'runname__experiment__project', 'datatype').get()
-    raws = filemodels.RawFile.objects.filter(datasetrawfile__dataset_id=dataset_id)
-    info['rawfiles'] = [x.name for x in raws]
-    storedfiles = {}
-    primaryfiles = [x.filename for x in files if x.filetype == 'raw']
-    if dset.datatype_id not in nonms_dtypes:
-        storedfiles['raw'] = primaryfiles
-        storedfiles['mzML'] = [x.filename for x in files if x.filetype == 'mzml'
-                               and x.checked]
-    else:
-        storedfiles[nonms_dtypes[dset.datatype_id]] = primaryfiles
-    info['storedfiles'] = storedfiles
-    info['backupfiles'] = [x.id for x in filemodels.SwestoreBackedupFile.objects.filter(
-        storedfile__rawfile__datasetrawfile__dataset_id=dataset_id)]
-    info['dset'] = {'id': dset.id, 'type': dset.datatype.name,
-                    'is_corefac': dset.runname.experiment.project.corefac,
-                    'storage_location': dset.storage_loc,
-                    'project': dset.runname.experiment.project.name,
-                    'experiment': dset.runname.experiment.name,
-                    'runname': dset.runname.name,
-                    'compstates': {x.dtcomp.component.name: x.state for x in
-                                   dsmodels.DatasetComponentState.objects.filter(
-                                       dataset_id=dataset_id).select_related(
-                                           'dtcomp__component')}
-                    }
-    dsjobs = filemodels.FileJob.objects.select_related('job', 'storedfile__rawfile__datasetrawfile').exclude(job__state=jobs.Jobstates.DONE).filter(storedfile__rawfile__datasetrawfile__dataset_id=dataset_id).distinct('job')
-
-    info['jobnames'] = [x.job.funcname for x in dsjobs]
+        'runname__experiment__project').get()
+    dsjobs = jm.Job.objects.exclude(state=jobs.Jobstates.DONE).filter(
+        filejob__storedfile__rawfile__datasetrawfile__dataset_id=dataset_id)
     info['jobs'] = [{'name': x.job.funcname, 'state': x.job.state,
                      'retry': jobs.is_job_retryable(x.job), 'id': x.job.id,
                      'time': x.job.timestamp} for x in dsjobs]
+    raws = filemodels.RawFile.objects.filter(datasetrawfile__dataset_id=dataset_id)
+    info['nrrawfiles'] = raws.count()
+    storedfiles = {}
+    files = filemodels.StoredFile.objects.filter(
+        rawfile__datasetrawfile__dataset_id=dataset_id)
+    if dset.datatype_id not in nonms_dtypes:
+        storedfiles['raw'] = files.filter(filetype='raw').count()
+        storedfiles['mzML'] = files.filter(filetype='mzml', checked=True).count()
+        if storedfiles['mzML'] == storedfiles['raw']:
+            info['mzmlable'] = False
+        elif 'convert_mzml' in [x.name for x in info['jobs']]:
+            info['mzmlable'] = 'blocked'
+        else:
+            info['mzmlable'] = 'ready'
+    else:
+        storedfiles[nonms_dtypes[dset.datatype_id]] = files.filter(filetype='raw').count()
+    info['nrstoredfiles'] = storedfiles
+    info['nrbackupfiles'] = filemodels.SwestoreBackedupFile.objects.filter(
+        storedfile__rawfile__datasetrawfile__dataset_id=dataset_id).count()
+    info['storage_location'] = dset.storage_loc
+    info['compstates'] = {x.dtcomp.component.name: x.state for x in
+                          dsmodels.DatasetComponentState.objects.filter(
+                              dataset_id=dataset_id).select_related(
+                                  'dtcomp__component')}
     return JsonResponse(info)
 
 
