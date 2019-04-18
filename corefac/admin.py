e7997b67293100f4d829acf1f2f20bb14471f579
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2019-04-18 17:21:38 +0200

    Removal of corefac django app

--- corefac/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from corefac.models import CorefacUser
-
-admin.site.register(CorefacUser)
--- corefac/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class CorefacUser(models.Model):
-    user = models.ForeignKey(User)
