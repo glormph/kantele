379d20a41ddd83229f40e955ed438e29b34e3f0c
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2020-11-10 15:33:56 +0100

    Move to path for rawstatus urls.py

--- rawstatus/urls.py
+++ rawstatus/urls.py
@@ -1,22 +1,22 @@
-from django.conf.urls import url
+from django.urls import path
 from rawstatus import views
 
 
 app_name = 'files'
 urlpatterns = [
-    url(r'^$', views.show_files, name='latestfiles'),
-    url(r'^instruments/$', views.instrument_page, name='instruments'),
-    url(r'^instruments/download/$', views.download_instrument_package),
-    url(r'^register/$', views.register_file, name='register'),
-    url(r'^register/userfile/$', views.register_userupload, name='registeruserupload'),
-    url(r'^userfile/$', views.request_token_userupload, name='req_userupload'),
-    url(r'^upload/token/$', views.upload_userfile_token, name='upload_userfile_token'),
-    url(r'^upload/userfile/$', views.browser_userupload, name='upload_browserfile'),
-    url(r'^transferred/$', views.file_transferred, name='transferred'),
-    url(r'^md5/$', views.check_md5_success, name='md5check'),
-    url(r'^md5/userfile/$', views.check_md5_success_userfile, name='md5checkuserfile'),
-    url(r'^setlibrary/$', views.set_libraryfile, name='setlibfile'),
-    url(r'^libfile/$', views.check_libraryfile_ready, name='checklibfile'),
-    url(r'^rename/$', views.rename_file),
-    url(r'^cleanup/$', views.cleanup_old_files),
+   path('', views.show_files, name='latestfiles'),
+   path('instruments', views.instrument_page, name='instruments'),
+   path('instruments/download', views.download_instrument_package),
+   path('register', views.register_file, name='register'),
+   path('register/userfile', views.register_userupload, name='registeruserupload'),
+   path('userfile', views.request_token_userupload, name='req_userupload'),
+   path('upload/token', views.upload_userfile_token, name='upload_userfile_token'),
+   path('upload/userfile', views.browser_userupload, name='upload_browserfile'),
+   path('transferred', views.file_transferred, name='transferred'),
+   path('md5', views.check_md5_success, name='md5check'),
+   path('md5/userfile', views.check_md5_success_userfile, name='md5checkuserfile'),
+   path('setlibrary', views.set_libraryfile, name='setlibfile'),
+   path('libfile', views.check_libraryfile_ready, name='checklibfile'),
+   path('rename', views.rename_file),
+   path('cleanup', views.cleanup_old_files),
 ]
