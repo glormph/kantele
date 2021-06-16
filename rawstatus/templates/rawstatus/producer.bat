1b15348fbf676e6ab3c12b86e244084e8319dba9
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2021-06-16 10:54:24 +0200

    Also change bat file for windows production uploaders

--- rawstatus/templates/rawstatus/producer.bat
+++ rawstatus/templates/rawstatus/producer.bat
@@ -5,13 +5,13 @@ set RAW_IS_FOLDER={{ is_folder }}
 set OUTBOX={{ datadisk }}\outbox
 set ZIPBOX={{ datadisk }}\zipbox
 set DONEBOX={{ datadisk }}\donebox
-set CLIENT_ID={{ client_id }}
 set KANTELEHOST={{ host }}
 set KEYFILE={{ key }}
 set SCP_FULL={{ scp_full }}
+set HOSTNAME={{ producerhostname }}
 
 REM change dir to script dir
 cd %~dp0
 
 call venv\Scripts\activate
-python.exe producer.py
+python.exe producer.py --client {{ client_id }}
