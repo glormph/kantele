0b5a44e80f9ad46d23fdb94ce7330c35bae6d691
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2022-10-05 11:53:53 +0200

    Extra logging in test script for github actions

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,19 @@
 # remove old test results if needed
 
+docker-compose --version
+echo Cleaning up
 git clean -xf data/test
 git checkout -- data/test
 
 # Clean old containers
 docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml down
 
+echo Prebuilding DB and MQ containers
 # Get DB container ready so web doesnt try to connect before it has init'ed
 docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml create db mq
 docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml start db
-sleep 5 && docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml stop db mq
+sleep 5 && docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml stop db
 
+echo Running tests
 # Run tests
 docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml run --use-aliases web python manage.py test kantele
