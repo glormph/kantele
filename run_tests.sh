fdd77d86cd175240ee99ea59a3689a39241c8e64
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2022-10-06 14:03:24 +0200

    Use compose v2 on github actions (.env vars seem interpolated into testing.env) and exit with 1 if tests fail

--- run_tests.sh
+++ run_tests.sh
@@ -6,11 +6,11 @@ git clean -xf data/test
 git checkout -- data/test
 
 # Clean old containers
-docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml down
+docker compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml down
 
 echo Prebuilding DB and MQ containers
 # Get DB container ready so web doesnt try to connect before it has init'ed
-docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml up --detach db mq
+docker compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml up --detach db mq
 echo Created db container and started it
 sleep 5
 
@@ -18,4 +18,4 @@ echo Running tests
 export GROUP_ID=$(id -g)
 export USER_ID=$(id -u)
 # Run tests
-docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml run --use-aliases web python manage.py test kantele || docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml logs storage_mvfiles storage_downloads
+docker compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml run --use-aliases web python manage.py test kantele || (docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml logs storage_mvfiles storage_downloads && exit 1)
