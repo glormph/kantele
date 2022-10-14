53173215f820a138bae52a54a80cbffdae3a80de
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2022-10-14 10:59:45 +0200

    Test for job view udate storage loc of file

--- src/backend/jobs/tests.py
+++ src/backend/jobs/tests.py
@@ -90,3 +90,64 @@ class TestUpdateStorageLocDset(BaseJobTest):
         self.assertEqual(self.ds1.storage_loc, newstorloc)
         self.task.refresh_from_db()
         self.assertEqual(self.task.state, cstates.SUCCESS)
+
+
+class TestUpdateStorageLocFile(BaseJobTest):
+    url = '/jobs/set/storagepath/'
+    # multiple jobs use this, but take this job
+    jobname = 'move_dset_servershare'
+
+    def setUp(self):
+        super().setUp()
+        self.ft = rm.StoredFileType.objects.create(name='testft', filetype='tst')
+        self.prod = rm.Producer.objects.create(name='prod1', client_id='abcdefg', shortname='p1')
+        self.raw = rm.RawFile.objects.create(name='file1', producer=self.prod,
+                source_md5='b7d55c322fa09ecd8bea141082c5419d',
+                size=100, date=timezone.now(), claimed=False)
+        self.sf = rm.StoredFile.objects.create(rawfile=self.raw, filename=self.raw.name, servershare=self.ss,
+                path='', md5=self.raw.source_md5, checked=False, filetype=self.ft)
+
+    def test_wrong_client(self):
+        resp = self.cl.post(self.url, content_type='application/json',
+                data={'client_id': 'fake'})
+        self.assertEqual(resp.status_code, 403)
+        resp = self.cl.post(self.url, content_type='application/json',
+            data={'no_client_id': 1})
+        self.assertEqual(resp.status_code, 403)
+
+    def test_one_fnid(self):
+        newshare = rm.ServerShare.objects.create(name='newshare', server=self.fserver, share='/')
+        resp = self.cl.post(self.url, content_type='application/json',
+                data={'client_id': settings.ANALYSISCLIENT_APIKEY, 'fn_id': self.sf.pk,
+                    'dst_path': 'new_path', 'servershare': newshare.name, 'task': self.taskid,
+                    'newname': 'newfilename',
+                    })
+        self.assertEqual(resp.status_code, 200)
+        self.sf.refresh_from_db()
+        self.assertEqual(self.sf.path, 'new_path')
+        self.assertEqual(self.sf.servershare, newshare)
+        self.assertEqual(self.sf.filename, 'newfilename')
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.state, 'SUCCESS')
+         
+    def test_multifiles(self):
+        raw2 = rm.RawFile.objects.create(name='file2', producer=self.prod,
+                source_md5='r328j9dqhj32qh98ddh3982q',
+                size=100, date=timezone.now(), claimed=False)
+        sf2 = rm.StoredFile.objects.create(rawfile=raw2, filename=raw2.name, servershare=self.ss,
+                path='', md5=raw2.source_md5, checked=False, filetype=self.ft)
+        newshare = rm.ServerShare.objects.create(name='newshare', server=self.fserver, share='/')
+        resp = self.cl.post(self.url, content_type='application/json',
+                data={'client_id': settings.ANALYSISCLIENT_APIKEY, 'fn_ids': [self.sf.pk, sf2.pk],
+                    'dst_path': 'new_path', 'servershare': newshare.name, 'task': self.taskid,
+                    'newname': 'newfilename',
+                    })
+        self.assertEqual(resp.status_code, 200)
+        self.sf.refresh_from_db()
+        self.assertEqual(self.sf.path, 'new_path')
+        self.assertEqual(self.sf.servershare, newshare)
+        sf2.refresh_from_db()
+        self.assertEqual(sf2.path, 'new_path')
+        self.assertEqual(sf2.servershare, newshare)
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.state, 'SUCCESS')
