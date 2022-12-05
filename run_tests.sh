e004220940a94df28d1e5c6572fef7932aebe763
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2022-12-05 18:21:41 +0100

    Added linting and testing specific modules to test bash script

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,14 @@
-set -euo pipefail
+set -eo pipefail
+
+
+if [[ -z "$1" ]]
+then
+    TESTCMD="python manage.py test"
+    echo will run empty $TESTCMD
+else
+    TESTCMD="python manage.py test $1"
+    echo will run $TESTCMD
+fi
 
 # remove old test results if needed (locally)
 echo Cleaning up
@@ -7,6 +17,19 @@ git checkout -- data/fakestorage
 
 # Clean old containers
 docker compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml down
+
+# Lint seems to operate on the local dir
+echo Running linting
+docker compose --env-file src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml run web pylint -E --disable E1101,E0307 --ignore-paths '.*\/migrations\/[0-9]+.*.py' \
+   analysis \
+   datasets \
+   dashboard \
+   home \
+   jobs \
+   kantele \
+   rawstatus \
+   || (echo Linting failed && exit 2)
+echo Linting OK
 
 echo Prebuilding DB and MQ containers
 # Get DB container ready so web doesnt try to connect before it has init'ed
@@ -18,5 +41,5 @@ echo Running tests
 export GROUP_ID=$(id -g)
 export USER_ID=$(id -u)
 # Run tests
-docker compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml run --use-aliases web python manage.py test || (docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml logs storage_mvfiles storage_downloads && exit 1)
+docker compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml run --use-aliases web $TESTCMD || (docker-compose --env-file  src/docker/.compose.testing.env -f src/docker/docker-compose-testing.yml logs storage_mvfiles storage_downloads && exit 1)
 
