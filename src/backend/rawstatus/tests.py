6b8dfea90968ad34db95e2c0e37289288330f6d3
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2023-07-20 11:15:02 +0200

    Fix test size of edited/downloaded file had changed

--- src/backend/rawstatus/tests.py
+++ src/backend/rawstatus/tests.py
@@ -415,7 +415,7 @@ class TestDownloadUploadScripts(BaseFilesTest):
     url = '/files/datainflow/download/'
     zipsizes = {'kantele_upload.sh': 337,
             'kantele_upload.bat': 185,
-            'upload.py': 26106,
+            'upload.py': 26072,
             'transfer.bat': 177,
             'transfer_config.json': 202,
             'setup.bat': 689,
