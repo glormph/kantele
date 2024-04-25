4aa2f85f39b44b4068b4c81271dfc03b8796a8e0
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2024-04-25 16:05:53 +0200

    Added run log/token for refine in jobs

--- src/backend/home/views.py
+++ src/backend/home/views.py
@@ -947,8 +947,12 @@ def refine_mzmls(request):
     # Refine data
     # FIXME get analysis if it does exist, in case someone reruns?
     analysis = anmodels.Analysis.objects.create(user=request.user, name=f'refine_dataset_{dset.pk}')
-    jj.create_job('refine_mzmls', dset_id=dset.pk, analysis_id=analysis.id, wfv_id=data['wfid'],
+    job = jj.create_job('refine_mzmls', dset_id=dset.pk, analysis_id=analysis.id, wfv_id=data['wfid'],
             dstshare_id=res_share.pk, dbfn_id=data['dbid'], qtype=dset.quantdataset.quanttype.shortname)
+    uwf = anmodels.UserWorkflow.objects.get(nfwfversionparamset_id=data['wfid'],
+            wftype=anmodels.UserWorkflow.WFTypeChoices.SPEC)
+    anmodels.NextflowSearch.objects.update_or_create(analysis=analysis, defaults={
+        'nfwfversionparamset_id': data['wfid'], 'job_id': job.id, 'workflow_id': uwf.pk, 'token': ''})
     return JsonResponse({})
 
 
